f0d0d791db8d39ce94a87820e9e4f2180e7530ee
tabidots <tabidots@gmail.com>
2017-02-05 17:06:32 +0800

    First draft of "topActiveUsers" endpoint
    Output conforms to the spec.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var bluebird = require('bluebird')
+var moment = require('moment')
 var pgp = require('pg-promise')({
   promiseLib: bluebird
 })
@@ -8,16 +9,16 @@ var join = bluebird.join
 
 require('dotenv').load()
 var db = pgp({
-  user: process.env.JOBBATICAL_USER,
-  password: process.env.JOBBATICAL_PW,
-  database: process.env.JOBBATICAL_DB,
-  host: process.env.JOBBATICAL_HOST,
+  user: 'justin' || process.env.JOBBATICAL_USER,
+  password: '' || process.env.JOBBATICAL_PW,
+  database: 'justin' || process.env.JOBBATICAL_DB,
+  host: 'localhost' || process.env.JOBBATICAL_HOST,
   port: 5432,
   max: 10,
   idleTimeoutMillis: 1000
 })
 
-app.get('/users', function(req, res){
+app.get('/users', function(req, res) {
   var id = parseInt(req.query.id)
   var payload = {
     companies: [],
@@ -28,7 +29,7 @@ app.get('/users', function(req, res){
   db.task(function(t) {
 
     return t.one('SELECT * FROM users WHERE id = $1', id)
-      .then(function(user){
+      .then(function(user) {
         if (!user) {
           return res.json({}) // TODO: Replace with error message
         }
@@ -39,22 +40,22 @@ app.get('/users', function(req, res){
         })
         return user
 
-    }).then(function(user){
+    }).then(function(user) {
 
       // Cannot get the company info directly; need to get IDs from teams table first
       return t.result('SELECT company_id, contact_user FROM teams WHERE user_id = $1 LIMIT 5', user.id)
-        .then(function(companyInfoByUser){
+        .then(function(companyInfoByUser) {
 
-          var companyIds = companyInfoByUser.rows.map(function(x){
+          var companyIds = companyInfoByUser.rows.map(function(x) {
             return x.company_id
           })
           return t.any('SELECT * FROM companies WHERE id = ANY($1::int[])', [companyIds]) // TODO: Limit to 5
-            .then(function(companies){
+            .then(function(companies) {
 
               // Is the user a contact for each company they are associated with?
-              for (company in companies){
+              for (company in companies) {
                 var thisCompany = companies[company]
-                var miscInfo = companyInfoByUser.rows.find(function(o){
+                var miscInfo = companyInfoByUser.rows.find(function(o) {
                   return o.company_id === thisCompany.id
                 })
                 Object.assign(thisCompany, miscInfo)
@@ -71,12 +72,12 @@ app.get('/users', function(req, res){
             })
         })
 
-    }).then(function(user){
+    }).then(function(user) {
 
       return t.any("SELECT * from listings WHERE created_by = $1 LIMIT 5", user.id) // TODO: sort?
-        .then(function(listings){
+        .then(function(listings) {
 
-          for (listing in listings){
+          for (listing in listings) {
             var thisListing = listings[listing]
             payload.createdListings.push({
               'id': payload.id,
@@ -87,21 +88,21 @@ app.get('/users', function(req, res){
 
           }
 
-          var listingIds = listings.map(function(x){
+          var listingIds = listings.map(function(x) {
             return x.id
           })
 
       return user
 
-    }).then(function(user){
+    }).then(function(user) {
 
       return t.any('SELECT * FROM applications WHERE user_id = $1', user.id) // TODO: Limit to 5
-        .then(function(applications){
+        .then(function(applications) {
 
-          for (application in applications){
+          for (application in applications) {
             var thisApplication = applications[application]
             return t.any('SELECT * FROM listings WHERE id = $1', thisApplication.listing_id)
-              .then(function(listing){
+              .then(function(listing) {
 
                 thisApplication.listing = listing[0]
                 thisApplication.coverLetter = thisApplication.cover_letter // minor editing
@@ -120,17 +121,75 @@ app.get('/users', function(req, res){
 
       })
 
-    }).then(function(user){
+    }).then(function(user) {
       res.json(payload)
-    }).catch(function(error){
-      // error
+    }).catch(function(error) {
+      // TODO: Catch error here
     })
 
   })
 })
 
-app.get('/topActiveUsers', function(req, res){
-  var pageNumber = req.query.page || 1
+app.get('/topActiveUsers', function(req, res) {
+  var pageNumber = parseInt(req.query.page) || 0
+  var payload = []
+
+  // 1. Determine what "last week" is
+  var endOfWeek = moment().format('YYYY/MM/DD')
+  var startOfWeek = moment().subtract(7, 'days').format('YYYY/MM/DD')
+
+  db.task(function(t) {
+
+    // 2. Query all applications in last week, sort by user occurrence
+    return t.any("SELECT user_id, COUNT(*) AS count FROM applications \
+                  WHERE created_at BETWEEN $1 AND $2 \
+                  GROUP BY user_id \
+                  ORDER BY count DESC, user_id \
+                  LIMIT 5 OFFSET $3", [startOfWeek, endOfWeek, 5 * pageNumber])
+
+      .then(function(topUsers) {
+
+        // 3. Inject user data into payload
+        return t.batch(topUsers.map(function(user) {
+          return t.one("SELECT * from users WHERE id = $1", user.user_id)
+            .then(function(user) {
+              var thisTopUser = topUsers.find(function(x) {
+                return x.user_id === user.id
+              })
+
+              payload.push({
+                'id': user.id,
+                'createdAt': user.created_at,
+                'name': user.name,
+                'count': thisTopUser.count,
+                listings: []
+              })
+
+              // 4. Take newest 3 listings for each user and inject into appropriate user
+              return t.any("SELECT name from listings \
+                            WHERE created_by = $1 \
+                            ORDER BY created_at DESC \
+                            LIMIT 3", user.id)
+                .then(function(listings) {
+                  var targetUser = payload.find(function(x) {
+                    return x.id === user.id
+                  })
+                  targetUser.listings = listings.map(function(x) {
+                    return x.name // we just want the names
+                  })
+                })
+
+            })
+          }) // end map
+        ) // end batch
+
+      })
+
+  }).then(function() {
+    res.json(payload)
+  }).catch(function(error) {
+    // TODO: Catch error here
+  })
 
 })
 
